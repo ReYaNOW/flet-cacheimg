8b9719eb847066448afad1ede7b600c6ed80960d
Sergei <reyangood@gmail.com>
2025-08-04 12:57:25 +0300

    fix in create_control.dart;

--- src/flutter/flet_cacheimg/lib/flet_cacheimg.dart
+++ src/flutter/flet_cacheimg/lib/flet_cacheimg.dart
@@ -1,3 +1,1 @@
-library flet_cacheimg;
-
-export "../src/create_control.dart" show createControl, ensureInitialized;
+d
--- src/flutter/flet_cacheimg/lib/src/create_control.dart
+++ src/flutter/flet_cacheimg/lib/src/create_control.dart
@@ -5,13 +5,18 @@ CreateControlFactory createControl = (CreateControlArgs args) {
   final type = args.control.type.trim().toLowerCase();
   print("createControleeeee called with type: '$type'");
 
-  if (type == "flet_cacheimg") {
-    print("return FletCacheImgControl");
-    return FletCacheImgControl(
-      parent: args.parent,
-      control: args.control,
-    );
-  }
+    if (type == "flet_cacheimg") {
+        print("return FletCacheImgControl");
+        return FletCacheImgControl(
+          key: args.key,
+          parent: args.parent,
+          children: args.children,
+          control: args.control,
+          parentDisabled: args.parentDisabled,
+          parentAdaptive: args.parentAdaptive,
+          backend: args.backend,
+        );
+    }
 
   print("No matchiiing control for type: '$type', returning null");
   return null;
